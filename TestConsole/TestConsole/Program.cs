5607e7c9fb73baac84255bd023866b8ce0a23e12
Humayun Ahmed <humayun.ahmed@exact.com>
2020-11-01 18:23:59 +0100

    starting

---
+++ TestConsole/TestConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestConsole
+{
+	class MergeNames
+	{
+		public static string[] UniqueNames(string[] names1, string[] names2)
+		{
+			Dictionary<string,bool> valuePairs=new Dictionary<string, bool>();
+			foreach (var item in names1)
+			{
+				valuePairs.Add(item,true);
+			}
+
+			foreach (var item in names2)
+			{
+				if(!valuePairs.ContainsKey(item))
+					valuePairs.Add(item, true);
+			}
+
+			return valuePairs.Keys.ToArray();
+		}
+		static void Main(string[] args)
+		{
+			string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+			string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+			Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+		}
+	}
+}
---
+++ TestConsole/TwoSumConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoSum
+{
+	class Program
+	{
+		public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
+		{
+			var newList = list.Where(x => x < sum + 1).ToList();
+			for (int i = 0; i < newList.Count; i++)
+			{
+				for (int j = 1; j < newList.Count; j++)
+				{
+					if (newList[i] + newList[j] == sum)
+					{
+						return new Tuple<int, int>(newList[i], newList[j]);
+					}
+				}
+			}
+
+			return null;
+		}
+
+		public static void Main(string[] args)
+		{
+			Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+			
+			if (indices != null)
+			{
+				Console.WriteLine(indices.Item1 + " " + indices.Item2);
+			}
+		}
+    }
+}
